728dc5dbd1f99cd6d5c1a6f82a6ffacec847a88e
Woody Zhang <woodylab@foxmail.com>
2023-02-11 14:51:08 +0800

    add woodylab-hdmiusb-d1 model
    Signed-off-by: Woody Zhang <woodylab@foxmail.com>

--- kvmd/apps/__init__.py
+++ kvmd/apps/__init__.py
@@ -573,7 +573,7 @@ def _get_config_scheme() -> dict:
         "otgnet": {
             "iface": {
                 "net":    Option("169.254.0.0/28", type=functools.partial(valid_net, v6=False)),
-                "ip_cmd": Option(["/usr/bin/ip"],  type=valid_command),
+                "ip_cmd": Option(["/usr/sbin/ip"],  type=valid_command),
             },
 
             "firewall": {
